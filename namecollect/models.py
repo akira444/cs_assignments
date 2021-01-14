94eb379cb896f302d54d4ee3c554e7b8bb253de8
Akira Yokoyama <akirayokoyama999@gmail.com>
2021-01-14 21:27:13 +0100

    assignment validation

--- namecollect/models.py
+++ namecollect/models.py
@@ -1,9 +1,18 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
-# Create your models here.
+# Class for Gender
+class Gender(models.Model):
+    gender = models.CharField(max_length=6)
+
+    def __str__(self):
+        return self.gender
+
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    age = models.PositiveIntegerField(validators=[MaxValueValidator(125, 'Age cannot be more than 125')], null=True)
+    gender = models.ForeignKey('Gender', on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.last_name
